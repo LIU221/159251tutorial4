0efe0b204cc4a15f9b5dcbd82d6fef86467770be
LIU221 <1021226989@qq.com>
2019-10-08 11:09:35 +0800

    first commit

---
+++ src/main/java/159251_tutorial/App.java
@@ -0,0 +1,25 @@
+package 159251_tutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        add(2,3);
+        subtract(5,6);
+    }
+    
+    public static int add(int a,int b) {
+    	int c=a+b;
+    	return c;
+    }
+    
+    public static int subtract(int a,int b) {
+    	int c=a+b;
+    	return c;
+    }
+}
